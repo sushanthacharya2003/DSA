61f40b5d178d48d274b34daa9882c17c07b4a5be
sushanthacharya2003 <acharyasushanth37@gmail.com>
2025-01-26 15:19:59 +0530

    gfg

---
+++ dsa alg/java dsa/nto1.java
@@ -0,0 +1,13 @@
+class nto1 {
+    static void fun(int n) {
+        if (n == 0) {
+            return;
+        }
+        System.out.print(n + " ");
+        fun(n - 1);
+    }
+
+    public static void main(String[] args) {
+        fun(8);
+    }
+}
